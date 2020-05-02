46c07aa11afd4b576da38c35380b82672d3560fc
kevinyohe <kevinyohe7@gmail.com>
2020-05-02 16:44:41 -0400

    https://angular.io/tutorial/toh-pt3

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Robot' },
+  { id: 12, name: 'Mean Machine' },
+  { id: 13, name: 'Lump' },
+  { id: 14, name: 'Alice' },
+  { id: 15, name: 'Cheshire' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
